72eee4829b67eb8792ee4a2e512f2075f20fee13
Mayank Talwar <dcmtalwar@gmail.com>
2020-10-16 22:44:50 +0530

    v2

--- Section-1/Prefix_Sum/Applications/subarray_0sum.cpp
+++ Section-1/Prefix_Sum/Applications/subarray_0sum.cpp
@@ -1,5 +1,7 @@
 //O(n^2) with just basic prefix sum usage
+//O(n) with prefix sum + hashing
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 bool check_for_sub(int a[], int n)
 {
@@ -10,15 +12,14 @@ bool check_for_sub(int a[], int n)
         prefixSum[i] = prefixSum[i - 1] + a[i];
     }
     
+    unordered_map<int, int> hashMap;
     for(int i = 0; i < n; i++)
     {
-        for(int j = i; j < n; j++)
+        if(prefixSum[i] == 0 || hashMap.find(prefixSum[i]) != hashMap.end()) //if a prefix sum ever repeats, zero sum subarray is found (MATHS)
         {
-            if(((i == 0) ? prefixSum[j] : prefixSum[j] - prefixSum[i-1]) == 0)
-            {
-                return true;
-            }
+            return true;
         }
+        hashMap[prefixSum[i]] = 1;
     }
 
     return false;
