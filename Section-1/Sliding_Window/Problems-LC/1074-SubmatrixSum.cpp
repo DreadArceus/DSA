4880d447a712c5b46e2af75fcc01ad6e3f19b14e
Mayank Talwar <dcmtalwar@gmail.com>
2020-11-05 11:02:24 +0530

    1074 N4

--- Section-1/Sliding_Window/Problems-LC/1074-SubmatrixSum.cpp
+++ Section-1/Sliding_Window/Problems-LC/1074-SubmatrixSum.cpp
@@ -1,51 +1,69 @@
-//Literally making a height-width variable sliding window
-//and sliding it around every single place it can be in a given matrix
-//insanity but kinda slow (passed 35/40 cases)
+//Second attempt, preprocessing some Prefix Sum arrays this time
+//Faster than the last one ofc, but still slower than needed (40/40 cases passed but took too long)
+//O(N^2 * M^2) or O(N^4) down from O(N^5)
+//From discussions, it seems changing the whole approach from 
+//  sliding window can make a passable O(N^4) solution
+//Thats coming up next
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int rangeSum(vector<vector<int>> matrix, int x1, int y1, int x2, int y2)
-{
-    int sum = 0;
-    for(int i = x1; i <= x2; i++)
-    {
-        for(int j = y1; j <= y2; j++)
-        {
-            sum += matrix[i][j];
-        }
-    }
-    return sum;
-}
-
 int solve(vector<vector<int>> matrix, int target)
 {
-    int cnt = 0;
-    for(int k1 = 1; k1 <= matrix.size(); k1++) //Dynamic Window Height
+    int n = matrix.size(), m = matrix[0].size();
+    vector<vector<int>> rowPS(n, vector<int>(m, 0));
+    vector<vector<int>> columnPS(n, vector<int>(m, 0));
+
+    for(int i = 0; i < n; i++)
     {
-        for(int k2 = 1; k2 <= matrix[0].size(); k2++) //Dynamic Window Width
+        rowPS[i][0] = matrix[i][0];
+        for(int j = 1; j < m; j++)
         {
-            int sum = rangeSum(matrix, 0, 0, k1 - 1, k2 - 1); //Initial Window of k1,k2 dimensions
+            rowPS[i][j] = rowPS[i][j-1] + matrix[i][j];
+        }
+    }
+    for(int j = 0; j < m; j++)
+    {
+        columnPS[0][j] = matrix[0][j];
+        for(int i = 1; i < n; i++)
+        {
+            columnPS[i][j] = columnPS[i-1][j] + matrix[i][j];
+        }
+    }
+
+    int cnt = 0;
+    for(int k1 = 1; k1 <= n; k1++) //Dynamic Window Height
+    {
+        for(int k2 = 1; k2 <= m; k2++) //Dynamic Window Width
+        {
+            //Initial Window of k1,k2 dimensions
+            int sum = 0;
+            for(int i = 0; i < k1; i++)
+            {
+                for(int j = 0; j < k2; j++)
+                {
+                    sum += matrix[i][j];
+                }
+            }
             if(sum == target)
             {
                 cnt++;
             }
 
-            for(int i = k2-1; i < matrix[0].size(); i++) //Moving window to the right
+            for(int i = k2-1; i < m; i++) //Moving window to the right
             {
                 if(i != k2-1)
                 {
-                    sum += rangeSum(matrix, 0, i, k1-1, i) - rangeSum(matrix, 0, i-k2, k1-1, i-k2);
+                    sum += columnPS[k1-1][i] - columnPS[k1-1][i-k2];
                     if(sum == target)
                     {
                         cnt++;
                     }
                 }
-
                 int localSum = sum;
-                for(int j = k1; j < matrix.size(); j++) //Moving window to the bottom
+                for(int j = k1; j < n; j++) //Moving window to the bottom
                 {
-                    localSum += rangeSum(matrix, j, i-k2+1, j, i) - rangeSum(matrix, j-k1, i-k2+1, j-k1, i);
+                    localSum += (rowPS[j][i] - ((i!=k2-1) ? rowPS[j][i-k2] : 0)) - (rowPS[j-k1][i] - ((i!=k2-1) ? rowPS[j-k1][i-k2] : 0));
                     if(localSum == target)
                     {
                         cnt++;
@@ -59,12 +77,12 @@ int solve(vector<vector<int>> matrix, int target)
 
 int main()
 {
-    int n = 0, target = 0;
-    cin >> n >> target;
+    int n = 0, m = 0, target = 0;
+    cin >> n >> m >> target;
     vector<vector<int>> grid(n);
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < m; j++)
         {
             int x = 0;
             cin >> x;
