fd2a7efb11bbaf4bb544b4cc02fe9b32ecca602a
Mayank Talwar <dreadarceus@gmail.com>
2020-12-18 10:23:08 +0530

    quick sort v2

--- Section-5/Quick_Sort/Demonstration.cpp
+++ Section-5/Quick_Sort/Demonstration.cpp
@@ -1,48 +1,49 @@
-//v1
-//My first implementation of quick sort.
-//The way i moved elements around the pivot seems to take too much time.
-//Hence this is not a good enough quick sort implementation
+//v2
+//Implementing the widely used quick sort logic
+//Used nested while loop, so test case #3 on HE still timed out
+//However the idea is accurate
 #include <iostream>
 #include <vector>
+#include <random>
 using namespace std;
 
 vector<int> quickSort(vector<int> v, int start, int end)
 {
-    if(start >= end - 1)
+    if(start >= end)
     {
         return v;
     }
 
-    int pivot = (end + start)/2;
-
-    for(int i = start; i < pivot; i++)
+    int pivot = rand() % (end - start + 1) + start;
+    int pivot_value = v[pivot];
+    v[pivot] = v[end];
+    v[end] = pivot_value;
+    pivot = end;
+    int l = start, r = end - 1;
+    while(true)
     {
-        auto it_current = v.begin() + i;
-        if(*it_current > *(v.begin() + pivot))
+        while(l < end && v[l] < pivot_value)
         {
-            int x = *(v.begin() + i);
-            v.erase(it_current);
-            pivot--;
-            v.insert(v.begin() + pivot + 1, x);
-            i--;
+            l++;
         }
-    }
-
-    for(int i = pivot + 1; i < end; i++)
-    {
-        auto it_current = v.begin() + i;
-        if(*it_current < *(v.begin() + pivot))
+        while(r >= start && v[r] >= pivot_value)
         {
-            int x = *(v.begin() + i);
-            v.erase(it_current);
-            v.insert(v.begin() + pivot, x);
-            pivot++;
-            i--;
+            r--;
         }
+        pivot = r + 1;
+        if(l >= r)
+        {
+            break;
+        }
+        int temp = v[l];
+        v[l] = v[r];
+        v[r] = temp;
+        pivot = r;
     }
+    v[end] = v[pivot];
+    v[pivot] = pivot_value;
 
-    v = quickSort(v, start, pivot);
-
+    v = quickSort(v, start, pivot - 1);
     v = quickSort(v, pivot + 1, end);
 
     return v;
@@ -51,6 +52,8 @@ vector<int> quickSort(vector<int> v, int start, int end)
 int main()
 {
     freopen("/Users/dreadarceus/github/DSA/Section-5/Quick_Sort/inp.txt", "r", stdin);
+    freopen("/Users/dreadarceus/github/DSA/Section-5/Quick_Sort/output.txt", "w", stdout);
+    srand(time(0));
     int n = 0;
     cin >> n;
     vector<int> v;
@@ -60,7 +63,7 @@ int main()
         cin >> x;
         v.push_back(x);
     }
-    v = quickSort(v, 0, n);
+    v = quickSort(v, 0, n - 1);
     for(int x : v)
     {
         cout << x << " ";
