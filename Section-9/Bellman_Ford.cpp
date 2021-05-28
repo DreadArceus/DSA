b01677441c9d54c1e23b7de077d196ed52b15c58
dreadarceus <dreadarceus@gmail.com>
2021-05-28 17:47:09 +0530

    Bellman Ford

---
+++ Section-9/Bellman_Ford.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+int32_t mod = 1e9 + 7;
+
+struct Edge
+{
+    int u, v, w;
+};
+vector<Edge> graph;
+vector<vector<int>> adj;
+vector<int> visited;
+void dfs(int source)
+{
+    visited[source] = true;
+    for (auto v : adj[source])
+        if (!visited[v])
+            dfs(v);
+}
+
+vector<int> bellman(int n, int m, int source)
+{
+    vector<int> d(n, INT64_MAX);
+    d[source] = 0;
+    bool going = true, cycle = false;
+    int cnt = 0;
+    while (going)
+    {
+        if (++cnt == n)
+        {
+            cycle = true;
+            break;
+        }
+        going = false;
+        for (int i = 0; i < m; i++)
+        {
+            int u = graph[i].u, v = graph[i].v, w = graph[i].w;
+            if (d[u] != INT64_MAX && d[v] > d[u] + w)
+                d[v] = d[u] + w, going = true;
+        }
+    }
+    if (cycle)
+    {
+        queue<int> q;
+        for (int i = 0; i < m; i++)
+        {
+            int u = graph[i].u, v = graph[i].v, w = graph[i].w;
+            if (d[u] != INT64_MAX && d[v] > d[u] + w)
+                d[v] = d[u] + w, q.push(v);
+        }
+        visited.resize(n);
+        while (!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+            if (visited[cur])
+                continue;
+            dfs(cur);
+            if (visited[n - 1])
+                return {};
+        }
+    }
+    return d;
+}
